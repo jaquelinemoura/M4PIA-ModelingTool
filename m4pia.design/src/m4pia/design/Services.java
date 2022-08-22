77f5be963cfa4ec69b04f41afd44385edf272f48
JaquelineMoura <moura.jak@gmail.com>
2022-08-22 05:00:36 -0300

    feat: Creation tools and DSL ajusts

--- m4pia.design/src/m4pia/design/Services.java
+++ m4pia.design/src/m4pia/design/Services.java
@@ -16,6 +16,9 @@ import M4PIAmetamodel.BasicType;
  */
 public class Services {
 
+	static String ICONS_PATH = "/m4pia.design/images/shapes/";
+	static String DEFAULT_ICON = "Cube-Blue.svg";	
+
 	/**
 	 * See
 	 * http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24
@@ -25,7 +28,7 @@ public class Services {
 		// TODO Auto-generated code
 		return self;
 	}
-
+	
 	/*********************** EQUIPMENT *******************************/
 
 	/**
@@ -35,7 +38,8 @@ public class Services {
 	 * @return String equipment's icon (valid) else default icon path  
 	 */
 	public String getEquipmentImage(Equipment equipment) {
-		String defaultIcon = "/m4pia.design/images/shapes/Box-Blue.svg";
+		
+		String defaultIcon = ICONS_PATH + DEFAULT_ICON;
 		
 		String equipmentIcon = equipment.getIcon();
 		if (equipmentIcon == null || equipmentIcon.isEmpty()) {
@@ -49,66 +53,64 @@ public class Services {
 
 		if (fileExtension.compareToIgnoreCase("png") == 0 || fileExtension.compareToIgnoreCase("svg") == 0
 				|| fileExtension.compareToIgnoreCase("jpg") == 0) {
-			return equipmentIcon;
+			return ICONS_PATH + equipmentIcon;
 		}
 
 		return defaultIcon;
 	}
-
+   
 	
 	public String getEquipmentImage(EquipmentType equipmentType) {
 		return getEquipmentImage(equipmentType.getBase());
 	}
 	/*********************** ATTRIBUTE *******************************/
-
+ 
 	/**
-	 * This method return a formated label to display with the Equipment Using
-	 * notation interval to represent Lower an Upper on Real and Integer e.g.
-	 * [Lower,Upper]
-	 * 
+	 * This method return a formated label to display with the Equipment details like notation interval
+	 *  Lower and Upper limits to Real and Integer e.g. * [Lower,Upper], TRUE/FALSE to boolean
 	 * @param attribute
-	 * @return
+	 * @return label
 	 */
 	public String getAttributeDetails(Attribute attribute) {		
-		String label = " ";
-		
-		// BasicType -> Typed-> Attribute -> Variable
+		String label = new String();
 		if (attribute instanceof BasicType) {
 			if (attribute instanceof Real) {
 				Real real = (Real) attribute;
-				label += real.getDefault() != null ? real.getDefault() : "";
-				label += real.getDisplayunit() != null ? real.getDisplayunit() : "";
 				if (real.getLower() != null || real.getUpper() != null) {
-					// label += " [" + real.getLower() + "," + real.getUpper() + "]";
 					label += real.getLower() != null ? " ["+real.getLower() : " ]-";
-					label += ",";
+					label += " , ";
 					label += real.getUpper() != null ? real.getUpper()+"]" : "+[";
+				}else {
+					label += " [" + real.getLower() + " , " + real.getUpper() + "]";
 				}
+				if (real.getDefault() != null ) {
+					label += " default: " + real.getDefault() + " ";
+					label += real.getDisplayunit() != null ? real.getDisplayunit() : "";													
+				}			
 			} else if (attribute instanceof Integer) {
 				Integer integer = (Integer) attribute;
-				// TO-DO solve because metamodel can't test is null
-				// label += integer.getDefault() != null ? integer.getDefault() : "";
-				label += integer.getDefault() + " [" + integer.getLower() + "," + integer.getUpper() + "]"; // exibia
 				boolean hasLowwer = String.valueOf(integer.getLower()) != null;
 				boolean hasUpper = String.valueOf(integer.getUpper()) != null;
-				if (hasLowwer || hasUpper) {
+				if (hasLowwer && hasUpper) {
+					label = integer.getDefault() + " [" + integer.getLower() + " , " + integer.getUpper() + "]";					
+				}else {
 					label += hasLowwer ? " ["+integer.getLower() : " ]-";
-					label += ",";
+					label += " , ";
 					label += hasUpper ? integer.getUpper()+ "]" : "+[";
 				}
 
 			} else if (attribute instanceof Boolean) {
 				Boolean bool = (Boolean) attribute;
 				if (bool.getDefault() != null) {
-					label += bool.getDefault() ? "TRUE" : "FALSE";
+					label = bool.getDefault() ? "TRUE" : "FALSE";
 				}
 			} else if (attribute instanceof M4PIAmetamodel.String) {
 				M4PIAmetamodel.String str = (M4PIAmetamodel.String) attribute;
 				if (str.getDefault() != null) {
-					label += str.getDefault();
+					label = str.getDefault();
 				}
 			}
-		} else { // EquipmentType
+		} else {
 			EquipmentType equipmentType = (EquipmentType) attribute;
 			label = attribute.getName() + ": " + equipmentType.getBase().getName();
 		}
